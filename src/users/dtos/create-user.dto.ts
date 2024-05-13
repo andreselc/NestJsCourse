3ac1343530f5ea8e37c00421019c581f9265d3a7
Andrés <95057542+andreselc@users.noreply.github.com>
2024-05-12 22:45:45 -0400

    Another change in DTO

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,11 +1,9 @@
 import {IsEmail, IsString} from "class-validator";
 
 export class CreateUserDto{
-
+  @IsEmail()
+  email: string;
+  
   @IsString()
-  name: string;
-
-  @IsEmail({}, { each: true }) 
-  emails: string[]; 
-
+  password: string;
 }
